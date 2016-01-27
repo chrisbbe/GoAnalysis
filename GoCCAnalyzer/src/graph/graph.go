1b789ee49e1102aa1e17391bb2aacb013638e948
Christian Bergum Bergersen <cbbx@computas.com>
2016-01-27 01:05:31 +0100

    MA-56 Tarjans algorithm for strongly connected components implemented

--- GoCCAnalyzer/src/graph/graph.go
+++ GoCCAnalyzer/src/graph/graph.go
@@ -1,8 +1,37 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2015-2016 Christian Bergum Bergersen
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package graph
 
+import (
+	"github.com/chrisbbe/GoAnalysis/Stack/src"
+)
+
 type Graph struct {
-	Root  *Node
-	Nodes map[interface{}]*Node
+	Root     *Node
+	Nodes    map[interface{}]*Node
+	scc      []*StronglyConnectedComponent
+	preCount int         // Used by SCC.
+	stack    stack.Stack // Used by SCC.
 }
 
 type Node struct {
@@ -10,9 +39,14 @@ type Node struct {
 	visited  bool
 	inEdges  []*Node
 	outEdges []*Node
+	low      int // Used by SCC.
 }
 
-func New() *Graph {
+type StronglyConnectedComponent struct {
+	Nodes []*Node
+}
+
+func NewGraph() *Graph {
 	return &Graph{Nodes: map[interface{}]*Node{}}
 }
 
@@ -24,8 +58,7 @@ func (n *Node) insertInNode(node *Node) {
 	n.inEdges = append(n.inEdges, node)
 }
 
-func (graph *Graph) InsertNode(leftNode *Node, rightNode *Node) {
-
+func (graph *Graph) InsertEdge(leftNode *Node, rightNode *Node) {
 	if len(graph.Nodes) == 0 {
 		graph.Root = leftNode
 		graph.Root.insertOutNode(rightNode)
@@ -33,7 +66,7 @@ func (graph *Graph) InsertNode(leftNode *Node, rightNode *Node) {
 		graph.Nodes[graph.Root.Value] = graph.Root
 		graph.Nodes[rightNode.Value] = rightNode
 	} else {
-		//Get right left and right node if they already exist.
+		//Get left and right node if they already exist.
 		if graph.Nodes[leftNode.Value] != nil {
 			leftNode = graph.Nodes[leftNode.Value]
 		}
@@ -58,10 +91,12 @@ func (node *Node) getDFS() (nodes []*Node) {
 	if !node.visited {
 		nodes = append(nodes, node)
 	}
-	for _, n := range node.outEdges {
-		nodes = append(nodes, n.getDFS()...)
-	}
 	node.visited = true
+	for _, n := range node.outEdges {
+		if !n.visited {
+			nodes = append(nodes, n.getDFS()...)
+		}
+	}
 	return nodes
 }
 
@@ -72,4 +107,73 @@ func (graph *Graph) GetDFS() (nodes []*Node) {
 		node.visited = false
 	}
 	return nodes
+}
+
+func (graph *Graph) dfs(v *Node) {
+	v.low = graph.preCount
+	graph.preCount++
+	v.visited = true
+	graph.stack.Push(v)
+
+	min := v.low
+	for _, w := range v.outEdges {
+		if !w.visited {
+			graph.dfs(w)
+		}
+		if w.low < min {
+			min = w.low
+		}
+	}
+
+	if min < v.low {
+		v.low = min
+		return
+	}
+
+	component := []*Node{}
+	var w interface{}
+	for ok := true; ok; ok = w.(*Node) != v {
+		w, _ = graph.stack.Pop()
+		component = append(component, w.(*Node))
+		w.(*Node).low = len(graph.Nodes) - 1
+	}
+
+	graph.scc = append(graph.scc, &StronglyConnectedComponent{Nodes:component})
+}
+
+func (graph *Graph) GetSCComponents() ([]*StronglyConnectedComponent) {
+	graph.scc = []*StronglyConnectedComponent{} //Init list.
+	for _, node := range graph.Nodes {
+		if !node.visited {
+			graph.dfs(node)
+		}
+	}
+	//Clean up nodes by setting visited = false
+	for _, node := range graph.Nodes {
+		node.visited = false
+		node.low = 0
+	}
+	//Clean up graph.
+	graph.stack = stack.Stack{}
+	graph.preCount = 0
+	return graph.scc
+}
+
+func (graph *Graph) GetNumberOfNodes() (int) {
+	return len(graph.Nodes)
+}
+
+func (graph *Graph) GetNumberOfEdges() (numberOfEdges int) {
+	for _, node := range graph.Nodes {
+		numberOfEdges += len(node.outEdges)
+	}
+	return numberOfEdges
+}
+
+func (graph *Graph) GetNumberOfSCComponents() (int) {
+	//We don't want to run Tarjan's once again if algorithm is already executed.
+	if graph.scc == nil {
+		return len(graph.GetSCComponents())
+	}
+	return len(graph.scc)
 }
--- GoCCAnalyzer/src/graph/graph_test.go
+++ GoCCAnalyzer/src/graph/graph_test.go
@@ -1,9 +1,30 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2015-2016 Christian Bergum Bergersen
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package graph
 
 import (
 	"testing"
 )
-
 
 func TestDepthFirstSearchInGraph(t *testing.T) {
 	//Create some nodes.
@@ -16,17 +37,17 @@ func TestDepthFirstSearchInGraph(t *testing.T) {
 	g := Node{Value:"G"}
 	h := Node{Value:"H"}
 
-	graph := New()
+	graph := NewGraph()
 
 	//Add directed node-pairs to graph.
-	graph.InsertNode(&a, &b)
-	graph.InsertNode(&a, &d)
-	graph.InsertNode(&b, &d)
-	graph.InsertNode(&b, &c)
-	graph.InsertNode(&c, &e)
-	graph.InsertNode(&e, &g)
-	graph.InsertNode(&e, &f)
-	graph.InsertNode(&f, &h)
+	graph.InsertEdge(&a, &b)
+	graph.InsertEdge(&a, &d)
+	graph.InsertEdge(&b, &d)
+	graph.InsertEdge(&b, &c)
+	graph.InsertEdge(&c, &e)
+	graph.InsertEdge(&e, &g)
+	graph.InsertEdge(&e, &f)
+	graph.InsertEdge(&f, &h)
 
 	dfs := graph.GetDFS()
 	correctDfs := []string{"A", "B", "D", "C", "E", "G", "F", "H"}
@@ -41,4 +62,62 @@ func TestDepthFirstSearchInGraph(t *testing.T) {
 			t.Errorf("Index %d in DFS-list is %s, should be %s!\n", i, dfs[i].Value, correctDfs[i])
 		}
 	}
+}
+
+func TestStronglyConnectedComponentsInGraph(t *testing.T) {
+	graph := NewGraph()
+
+	a := &Node{Value:0}
+	b := &Node{Value:1}
+	c := &Node{Value:2}
+	d := &Node{Value:3}
+	e := &Node{Value:4}
+	f := &Node{Value:5}
+	g := &Node{Value:6}
+	h := &Node{Value:7}
+
+	graph.InsertEdge(a, b)
+	graph.InsertEdge(b, c)
+	graph.InsertEdge(c, d)
+	graph.InsertEdge(d, c)
+	graph.InsertEdge(d, h)
+	graph.InsertEdge(h, d)
+	graph.InsertEdge(c, g)
+	graph.InsertEdge(h, g)
+	graph.InsertEdge(f, g)
+	graph.InsertEdge(g, f)
+	graph.InsertEdge(b, f)
+	graph.InsertEdge(e, f)
+	graph.InsertEdge(e, a)
+	graph.InsertEdge(b, e)
+
+	expectedStronglyConnectedComponents := graph.GetSCComponents()
+	actualStronglyConnectedComponents := []StronglyConnectedComponent{
+		StronglyConnectedComponent{Nodes:[]*Node{&Node{Value:5}, &Node{Value:6}}},
+		StronglyConnectedComponent{Nodes:[]*Node{&Node{Value:7}, &Node{Value:2}, &Node{Value:3}}},
+		StronglyConnectedComponent{Nodes:[]*Node{&Node{Value:1}, &Node{Value:0}, &Node{Value:4}}},
+	}
+
+	if len(expectedStronglyConnectedComponents) != len(actualStronglyConnectedComponents) {
+		t.Fatalf("Number of strongly connected components be %d, but are %d!\n", len(actualStronglyConnectedComponents),
+			len(expectedStronglyConnectedComponents))
+	}
+
+	for i, e := range actualStronglyConnectedComponents {
+		for _, f := range e.Nodes { //Hver eneste variabel i i
+			if !containsNodeValue(f.Value, expectedStronglyConnectedComponents[i].Nodes) {
+				t.Fatalf("Strongly connected component nr. %d should contain value %v!\n", i, f.Value)
+			}
+		}
+	}
+}
+
+//Helper function to test if a list of Nodes contains a value.
+func containsNodeValue(value interface{}, nodes []*Node) (bool) {
+	for _, v := range nodes {
+		if v.Value == value {
+			return true
+		}
+	}
+	return false
 }
