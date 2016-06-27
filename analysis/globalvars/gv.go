363cff1e82ccb961263182f1df3d64e060374a91
Christian Bergum Bergersen <cbb@computas.com>
2016-06-27 08:49:15 +0200

    Cleaning

--- analysis/globalvars/gv.go
+++
@@ -1,9 +0,0 @@
-// Package globalvars (Global Variables) provides shared global constants used in different modules.
-package globalvars
-
-// Miscellaneous.
-const (
-	PROGRAM_NAME = "GoAnalysis"
-	VERSION = "0.1"
-	WEBSITE = "http://chrisbbe.github.io/GoAnalysis/"
-)
